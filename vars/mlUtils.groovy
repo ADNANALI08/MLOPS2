2bb5bc6c2d862f725417bdc4f40159ae77406686
ADNAN <70148007@student.uol.edu.pk>
2026-05-13 02:50:43 +0500

    Fix Python path to use venv

--- vars/mlUtils.groovy
+++ vars/mlUtils.groovy
@@ -1,25 +1,28 @@
+
+def PYTHON = '/home/dell/MLOPS2/venv/bin/python3'
+
 def ingestData() {
-    sh 'python3 src/data_ingest.py'
+    sh "${PYTHON} src/data_ingest.py"
 }
 def trainModel() {
-    sh 'python3 src/train.py'
+    sh "${PYTHON} src/train.py"
 }
 def deployModel(String modelUri) {
-    sh "python3 src/deploy_model.py '${modelUri}'"
+    sh "${PYTHON} src/deploy_model.py '${modelUri}'"
 }
 def testModel(String modelUri) {
-    sh "python3 src/test_model.py '${modelUri}'"
+    sh "${PYTHON} src/test_model.py '${modelUri}'"
 }
 def registerModel(String alias) {
     def runId = readFile('run_id.txt').trim()
-    sh "python3 src/register_model.py '${runId}' '${alias}'"
+    sh "${PYTHON} src/register_model.py '${runId}' '${alias}'"
 }
 def loadModel(String alias) {
-    sh "python3 src/load_model.py '${alias}'"
+    sh "${PYTHON} src/load_model.py '${alias}'"
 }
 def updateAlias(String oldAlias, String newAlias) {
     def version = readFile('model_version.txt').trim()
-    sh "python3 src/update_alias.py '${version}' '${oldAlias}' '${newAlias}'"
+    sh "${PYTHON} src/update_alias.py '${version}' '${oldAlias}' '${newAlias}'"
 }
 def getRunId() {
     return readFile('run_id.txt').trim()
